d02010b859bb348b0d4b2580e19fbe74da17ea1a
vlad-solomon <48878155+vlad-solomon@users.noreply.github.com>
2020-10-12 17:48:25 +0300

    The cat's gonna vibe with you. What more could you ask for?

---
+++ app.js
@@ -0,0 +1,84 @@
+const container = $(".container")
+const loadVideo = $("#load-video")
+const resetButton = $("#reset");
+const togglePlay = $("#toggle-play")
+
+loadVideo.click(function(){
+
+    let videoLink = $("#text-field").val()
+    let videoLinkString = videoLink.toString().replace("youtu.be/" , "www.youtube.com/watch?v="); 
+    let videoId = videoLinkString.substring(32,43)
+    const condition = /www.youtube.com\/watch\?v=([a-zA-Z0-9\-_]+)/
+
+
+    if(videoLinkString.match(condition)){
+
+        $("#text-field").val("")
+
+        const video = $("iframe");
+        video.attr("src" , `https://www.youtube.com/embed/${videoId}?enablejsapi=1&playlist=${videoId}&autoplay=1&loop=1`)
+        
+        setTimeout(function(){
+            $("#dancing-cat")[0].play();
+            resetButton.css("display" , "block")
+            togglePlay.css("display" , "block")
+        }, 2000)
+    
+        container.children().css("display" , "none");
+        
+    } else {
+        alert("That link ain't valid bruv!")
+    };
+
+})
+
+resetButton.click(function(){
+    $("iframe").attr("src" , "");
+    $("#dancing-cat")[0].currentTime = 0;
+    $("#dancing-cat")[0].pause();
+    $(this).css("display" , "none")
+    togglePlay.css("display" , "none")
+    togglePlay.text("❚❚")
+    container.children().removeAttr("style")
+})
+
+function pauseYoutube(){
+    const iframes = $("iframe")
+    Array.prototype.forEach.call(iframes, iframe =>{
+        iframe.contentWindow.postMessage(JSON.stringify({
+            event: "command",
+            func: "pauseVideo"
+        }),
+        "*"
+        );
+    })
+}
+
+function resumeYoutube(){
+    const iframes = $("iframe")
+    Array.prototype.forEach.call(iframes, iframe =>{
+        iframe.contentWindow.postMessage(JSON.stringify({
+            event: "command",
+            func: "playVideo"
+        }),
+        "*"
+        );
+    })
+}
+
+let isVideoPlaying = true
+
+togglePlay.click(function(){
+    if(isVideoPlaying){
+        pauseYoutube();
+        $("#dancing-cat")[0].pause();
+        $(this).text("▶")
+        isVideoPlaying = false
+    }else{
+        resumeYoutube()
+        $(this).text("❚❚")
+        $("#dancing-cat")[0].play();
+        isVideoPlaying = true
+    }
+
+})
